c2f0d17faeb410fd13bbaa377f550b2eed6cff0b
Steffen Vogler <steffen.vogler@bayer.com>
2019-02-16 09:56:03 +0000

    wip

--- NLP_analysis_main.py
+++ NLP_analysis_main.py
@@ -10,9 +10,21 @@
 
 from util import *
 
-path = r"C:\Temp\DataScience_Team\DS_NLP_Assignment\sentences_with_sentiment.csv"
+# ARGUMENTS
+PATH = r"C:\Temp\DataScience_Team\DS_NLP_Assignment\sentences_with_sentiment.csv"
+WITH_PREPROCESSING = True
 
-df = read_list(path)
+# INPUT AND PARSING
+df = read_list(PATH)
+df_test = df['Sentence']
+sum_positive = df['Positive'].sum()
+sum_neutral = df['Neutral'].sum()
+sum_negative = df['Negative'].sum()
+print ("Positive sentences: {m}; Neutral sentences: {n}; Negative sentences: {o}; Total # of sentences: {p}"\
+       .format(m = sum_positive, n = sum_neutral, o = sum_negative, p = len(df)))
 
-print (df.head())
-print (len(df))
+# PRE-PROCESSING
+if WITH_PREPROCESSING == True:
+    
+
+print (df_test.head())
